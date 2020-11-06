fee0e394df6a52d9e252c5e435d4d96cd27762cd
EagleOnGitHub <imnuex@gmail.com>
2020-11-06 22:54:02 +0200

    New unstable directory, updated the readme, and added some files in that new directory

---
+++ unstable/wobmath.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2020 EagleOnGitHub, Vortetty
+
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// wobmath.h - math functions
+#include "wobdeclare.h"
+#include <stdlib.h>
+#include <time.h>
+
+int randint(void) {
+    return random();
+}
